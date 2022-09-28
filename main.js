aa57b9bfce51052623d16cb0fc020356a8a592c7
mohamedshalaash <112286468+mohamedshalaash@users.noreply.github.com>
2022-09-29 00:23:30 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,4 @@
+// bar in header in small width
+document.querySelector('.bar-icon-links').addEventListener('click',()=>{
+    document.querySelector('.bar-icon-links').classList.toggle('bar-icon-links-close')
+})
